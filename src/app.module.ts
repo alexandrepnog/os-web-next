d5278613393919efa705696ed9611216a0b74b5a
Alexandre <alexandre@gruposiac.com.br>
2020-03-20 17:06:53 -0300

    iniciando um projeto nest api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-//import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [],
-  // imports: [
-  //   MongooseModule.forRoot(
-  //      'mongodb+srv://admin:manager@cluster0-04j02.mongodb.net/webnelson?retryWrites=true&w=majority',
-  //     'mongodb+srv://douglas:27017/oswebnelson?retryWrites=true&w=majority',
-  //   ),
-  //   CatsModule,
-  // ],
+  imports: [
+    ConfigModule.forRoot({ expandVariables: true, isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_STRING, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
