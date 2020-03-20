a9636338e138891a8ec3fe4f2b6a4633a4134845
Alexandre <alexandre@gruposiac.com.br>
2020-03-20 15:05:54 -0300

    make it better

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:manager@cluster0-04j02.mongodb.net/webnelson?retryWrites=true&w=majority',
+    ),
+    CatsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
